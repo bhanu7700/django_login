6556900eecda6bb546f9bbe9840e6ce6d17f80e1
bhanu7700 <bhanu.udhissa@gmail.com>
2020-05-16 09:24:05 +0530

    first commit

---
+++ learning_user/basic_app/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from basic_app.models import UserProfileInfo
+
+
+
+
+# Register your models here.
+
+admin.site.register(UserProfileInfo)
---
+++ learning_user/basic_app/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from basic_app import views
+
+app_name = 'basic_app'
+
+urlpatterns = [
+
+    url('register/', views.register,name= 'register'),
+    url('user_login',views.user_login,name='user_login'),
+    
+    
+] 
